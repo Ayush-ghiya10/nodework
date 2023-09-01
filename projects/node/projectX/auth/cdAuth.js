4a8943845b072419cc8861a5b4c249a802bbcca3
Ayush-ghiya10 <ayush.ghiya@talentsystems.com>
2023-09-01 15:29:35 +0530

    Projects moved

--- projects/node/projectX/auth/cdAuth.js
+++
@@ -1,9 +0,0 @@
-module.exports = async(req,res,next)=>{
-    try {
-        const{payload:{userType}}=req;
-        if(!userType == 'cd') throw Error('Not Authorised');
-        next();
-    } catch (error) {
-        res.status(402).send({status:"Failed",message:error.message});
-    }
-}
